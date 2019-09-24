529f8fd2de1727c8b31a7f8d170e1aed17ccc647
OttoOctavius <octaviopompilio@gmail.com>
2019-09-24 17:46:11 -0300

    Se volvieron a incorporar varios archivos, incluyendo los del deploy

--- src/App.vue
+++ src/App.vue
@@ -16,27 +16,56 @@
     </v-app-bar>
 
     <v-content>
+      <!--
       <v-row>
         <v-col> <FormIngresoCliente /> </v-col>
         <v-col> <FormIngresoProveedor /> </v-col>
-      </v-row>
+      </v-row> 
+      -->
+
+      <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Vue Axios Post - ItSolutionStuff.com</div>
+                    <div class="card-body">
+                        <form @submit="formSubmit">
+                        <strong>Name:</strong>
+                        <input type="text" class="form-control" v-model="name">
+                        <strong>Description:</strong>
+                        <textarea class="form-control" v-model="description"></textarea>
+                        <button class="btn btn-success">Submit</button>
+                        </form>
+                        <strong>Output:</strong>
+                        <pre>  {{output}} </pre>
+                    </div>
+                    </div>
+            </div>
+        </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld';
 import FormIngresoCliente from './components/FormIngresoCliente';
 import FormIngresoProveedor from './components/FormIngresoProveedor';
-import Vue from 'vue'
-import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+let port = process.env.PORT || 8083;
+Vue.use(port);
 
-
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: HelloWorld }, // El origen es el template
+    { path: '/service/login', component: FormIngresoProveedor, props: true }, // Pass route.params to props
+    { path: '/client/login', component: FormIngresoCliente, props: { name: 'desconocido' }}, // static values
+  ]
+});
 
 export default {
-  name: 'App',
+  router,
+  name: "App",
   components: {
     FormIngresoCliente,FormIngresoProveedor
   },
